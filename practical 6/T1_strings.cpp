d903f16ea7ec50457b3a037a428528052fe35916
tanishqg0511 <91174393+tanishqg0511@users.noreply.github.com>
2021-10-06 09:45:33 +0530

    Create T1_strings.cpp

---
+++ practical 6/T1_strings.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=21;
+	float b=2.5;
+	string c="hello";
+	string d="world";
+	cout<<int+float<<endl;
+	cout<<int+str<<endl;
+	cout<<str+str<<endl;
+}
