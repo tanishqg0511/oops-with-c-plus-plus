0961d6092fcbc7cb8b94702477a22c196ab46db6
tanishqg0511 <91174393+tanishqg0511@users.noreply.github.com>
2021-10-22 19:47:17 +0530

    Update task1.cpp
    the reason of this error is that compiler is not allocating memory to pointer rather it is giving memory to pointer variable to store address.

--- practical 8/task1.cpp
+++ practical 8/task1.cpp
@@ -1,9 +1,9 @@
 #include<iostream>
 using namespace std;
 int main(){
-int *p={10,20,30};
-cout<<*p<<" " ;
-p++;
-cout<<*p;
-return 0;
+	int *p={10,20,30};
+	cout<<*p<<" " ;
+	p++;
+	cout<<*p;
+	return 0;
 }
