bf7c4b5040c4c8563ce3c87e1bd6a6e37700fcf0
tanishqg0511 <91174393+tanishqg0511@users.noreply.github.com>
2021-11-24 20:08:34 +0530

    Add files via upload

---
+++ practical_4/p4t4.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+namespace first
+{
+	void sum(int x,int y)
+	{
+		cout<<x+y<<endl;
+	}
+}
+namespace second
+{
+	void sum(float x,float y)
+	{
+		cout<<x+y<<endl;
+	}
+}
+using namespace first;
+using namespace second;
+int main()
+{
+	sum(5.5,8);
+	sum(6.4,8.5);
+	sum(5,6);
+}
+
