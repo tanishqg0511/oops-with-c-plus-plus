6b566c1dd4a2b5a8fb8e17edcfa9df315992f154
tanishqg0511 <91174393+tanishqg0511@users.noreply.github.com>
2021-12-07 14:34:57 +0530

    Create task2.cpp

---
+++ practical 11/task2.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+
+class A
+{
+	public:
+	int k;
+	void display()
+	{
+		cout<<"Running class A"<<endl;
+	}
+};
+class B:public A
+{
+	public:
+	void display1()
+	{
+		cout<<"Running class B"<<endl;
+	}
+};
+class C:public A
+{
+	public:
+	void display2()
+	{
+		cout<<"Running class C"<<endl;
+	}
+};
+class D: public B,public C
+{
+	public:
+	void display3()
+	{
+		cout<<"Running class D"<<endl;
+	}
+};
+int main()
+{
+	D d;
+	d.display1();
+	d.display2();
+	d.display3();
+	//d.display();
+	d.k=100;
+}
